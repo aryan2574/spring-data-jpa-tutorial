8cadca863b63f267c5872785ba6bd9e64548f35c
aryan2574 <ayush.aryan94@yahoo.com>
2024-02-17 18:38:36 +0530

    Added Entity relationship in SQL

---
+++ src/test/java/com/example/spring/data/jpa/tutorial/repository/CourseMaterialRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.spring.data.jpa.tutorial.repository;
+
+import com.example.spring.data.jpa.tutorial.entity.Course;
+import com.example.spring.data.jpa.tutorial.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseMaterialRepositoryTest {
+
+    @Autowired
+    private CourseMaterialRepository repository;
+
+    @Test
+    public void SaveCourseMaterial() {
+
+        Course course = Course.builder()
+                .title("DSA")
+                .credit(6)
+                .build();
+
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .url("www.google.com")
+                .course(course)
+                .build();
+
+        repository.save(courseMaterial);
+    }
+
+    @Test
+    public void printAllCourseMaterials() {
+        List<CourseMaterial> courseMaterials = repository.findAll();
+
+        System.out.println("Course Materials = " + courseMaterials);
+    }
+}
---
+++ src/test/java/com/example/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.example.spring.data.jpa.tutorial.repository;
+
+import com.example.spring.data.jpa.tutorial.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void printCourses() {
+        List<Course> courseList = courseRepository.findAll();
+
+        System.out.println("Courses = " + courseList);
+    }
+
+}
